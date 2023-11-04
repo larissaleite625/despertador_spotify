dea7fc3c04e5b894aadbe8d51f2be9f94d57ca82
Larissa Leite <86159676+larissaleite625@users.noreply.github.com>
2023-11-04 02:31:59 -0300

    Versão com a lista de músicas por personalidade

--- main.py
+++ main.py
@@ -2,30 +2,44 @@ import datetime
 import time
 from spotify import reproduzir_musica
 import os
+import random
 
 # Obter as credenciais do Spotify das variáveis de ambiente
 client_id = os.getenv('SPOTIPY_CLIENT_ID')
 print(os.getenv('SPOTIPY_CLIENT_SECRET'))
 client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
 
+
+leninista = ['0JRoo5kkeboyTWVdMEU3pw', '19RdiPkl18jXVfnvMaZBQV', '1dGaFOpHvz5fm45rQSYeKC', '5rRAOgZOAqHwUMwVAXkUYU', '4m7p2dejJrMiiA1vg8LDPK', '5ndylO3VZAhTRIARJFD0a5', '1CY57Jb3btf0qYXSqRC9Dm']
+rockista_comunista = ['712ukvLX20rwHuTWg7Gcjb', '59WN2psjkt1tyaxjspN8fp', '2SR0CHOWgkVt0nLI9YwYti'] 
+rockista_corna = ['6XimI1O15wpfwUdrCnlrxo', '5emb8RDOZtpXlw6D0eEQbz', '7eRG7KNDIgjIk3nvykmhOT'] 
+fa_de_diva_pop = ['1Hohk6AufHZOrrhMXZppax', '30XU4suKzCeoCK9YFzdufg', '1Bx0zEdVjkFlV27iKaePug']
+suicida = ['5DayxZpAgBVDCNFjQHWi4g', '3fjmSxt0PskST13CSdBUFx', '6olS0TmHmsGr0hXtcBsiVM']
+sambinha = ['']
+macumba = ['']
+racionais = ['']
+death_metal = ['']
+thrash_metal = ['']
+black_metal_nervoso = ['4bAHMUnHX1jMoLOpMSUF4w', '3P4Vcz7MhJngRf8WUAaoA0','6b0HdwX9k0RtOUHRvWYl4J']
+
+
+lista = [leninista, rockista_comunista, rockista_corna, fa_de_diva_pop, suicida, sambinha, macumba, racionais, death_metal, thrash_metal, black_metal_nervoso]
+lista_selecionada = random.choice(lista)
+musica = random.choice(lista_selecionada)
 # Defina a URI da música que deseja reproduzir
-music_uri = 'spotify:track:2HLY1qDaWKodXeR6FQJQux'
-
-#music_uri = 'spotify:track:0mKGwFMHzTprtS2vpR3b6s' 
-#ID de Cozy
-#https://open.spotify.com/intl-pt/track/0mKGwFMHzTprtS2vpR3b6s?si=d23274c76516460b
-#Link pra Cozy
-
+vai = 'spotify:track:' + str(musica)
+print(vai)
 
 # Defina o horário de despertar
-hora_despertar = datetime.datetime(2023, 6, 28, 17, 51)  #27 de junho de 2023, 7:30 AM
+hora_despertar = datetime.datetime(2023, 10, 6, 7, 0)  #ano, mes, dia, hora e min
 
-# Loop até o horário de despertar ser alcançado
+# Loop até o horário de despertar ser alcançado 
+
 while True:
     agora = datetime.datetime.now()
     if agora >= hora_despertar:
-        reproduzir_musica(music_uri)
+        reproduzir_musica(vai)
         break
     else:
-        print("Ainda não é hora de acordar. Verificando novamente em 10 min...")
-        time.sleep(600)  # Aguarda 10 min antes de verificar novamente
+        print("Amimir")
+        time.sleep(180)  # 3 min e ele verifica de novo
